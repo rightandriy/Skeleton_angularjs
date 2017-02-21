cacd5d9c7f39be7fa097478a0b6f0d54614af117
Unknown <andriy.privalov@gmail.com>
2017-02-21 17:29:25 +0200

    App.component
    App.component add

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
     imports: [ BrowserModule],  
-    declarations:  [ AppComponent ],
+    declarations:  [ 
+        AppComponent, 
+        CardComponent
+        ],
     bootstrap: [ AppComponent ]
 })
 
